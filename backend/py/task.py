c9dd7b0aeb3f665cd2d9a9549c014ec1263ed1dd
Leo-Zarni01 <leokhantzarni@gmail.com>
2024-02-25 03:08:33 +1100

    finished task 2

--- backend/py/task.py
+++ backend/py/task.py
@@ -63,6 +63,20 @@ def convert_files_to_graph(files: list[File]) -> Graph:
                     file_graph.add_edge(i, j)
     return file_graph
 
+def find_categories(files: list[File]) -> dict:
+    categories = {}
+    for each_file in files:
+        for each_category in each_file.categories:
+            if each_category not in categories:
+                categories[each_category] = 1
+            else:
+                categories[each_category] += 1
+    return categories
+
+def sort_categories(categories_dict: dict) -> dict:
+    return sorted(categories.items(), key=lambda x: (-x[1], x[0]))
+
+
 """
 Task 1
 """
@@ -77,9 +91,16 @@ def leafFiles(files: list[File]) -> list[str]:
 """
 Task 2
 """
-# def kLargestCategories(files: list[File], k: int) -> list[str]:
-#     return []
-
+def kLargestCategories(files: list[File], k: int) -> list[str]:
+    if k > len(files):
+        return [[i] for i in files]
+    categories = find_categories(files)
+    sorted_categories = sort_categories(categories)
+    res = []
+    for i in range(len(sorted_categories)):
+        if i <= k - 1: ## cuz index is 0!!!
+            res.append(sorted_categories[i])
+    return res
 
 """
 Task 3
@@ -87,24 +108,7 @@ Task 3
 # def largestFileSize(files: list[File]) -> int:
 #     return 0
 
-# testFiles = [File(1, "Document.txt", ["Documents"], 3, 1024),
-#         File(2, "Image.jpg", ["Media", "Photos"], 34, 2048),
-#         File(3, "Folder", ["Folder"], -1, 0),
-#         File(5, "Spreadsheet.xlsx", ["Documents", "Excel"], 3, 4096),
-#         File(8, "Backup.zip", ["Backup"], 233, 8192),
-#         File(13, "Presentation.pptx", ["Documents", "Presentation"], 3, 3072),
-#         File(21, "Video.mp4", ["Media", "Videos"], 34, 6144),
-#         File(34, "Folder2", ["Folder"], 3, 0),
-#         File(55, "Code.py", ["Programming"], -1, 1536),
-#         File(89, "Audio.mp3", ["Media", "Audio"], 34, 2560),
-#         File(144, "Spreadsheet2.xlsx", ["Documents", "Excel"], 3, 2048),
-#         File(233, "Folder3", ["Folder"], -1, 4096),]
-
-# leafFiles(testFiles)
-
-if __name__ == '__main__':
-    testFiles = [
-        File(1, "Document.txt", ["Documents"], 3, 1024),
+testFiles = [File(1, "Document.txt", ["Documents"], 3, 1024),
         File(2, "Image.jpg", ["Media", "Photos"], 34, 2048),
         File(3, "Folder", ["Folder"], -1, 0),
         File(5, "Spreadsheet.xlsx", ["Documents", "Excel"], 3, 4096),
@@ -115,23 +119,44 @@ if __name__ == '__main__':
         File(55, "Code.py", ["Programming"], -1, 1536),
         File(89, "Audio.mp3", ["Media", "Audio"], 34, 2560),
         File(144, "Spreadsheet2.xlsx", ["Documents", "Excel"], 3, 2048),
-        File(233, "Folder3", ["Folder"], -1, 4096),
-    ]
+        File(233, "Folder3", ["Folder"], -1, 4096),]
 
-    assert sorted(leafFiles(testFiles)) == [
-        "Audio.mp3",
-        "Backup.zip",
-        "Code.py",
-        "Document.txt",
-        "Image.jpg",
-        "Presentation.pptx",
-        "Spreadsheet.xlsx",
-        "Spreadsheet2.xlsx",
-        "Video.mp4"
-    ]
+categories = find_categories(testFiles)
+print(categories)
+print(sort_categories(categories))
+print(kLargestCategories(testFiles, 3))
+# leafFiles(testFiles)
 
-    # assert kLargestCategories(testFiles, 3) == [
-    #     "Documents", "Folder", "Media"
-    # ]
+# if __name__ == '__main__':
+#     testFiles = [
+#         File(1, "Document.txt", ["Documents"], 3, 1024),
+#         File(2, "Image.jpg", ["Media", "Photos"], 34, 2048),
+#         File(3, "Folder", ["Folder"], -1, 0),
+#         File(5, "Spreadsheet.xlsx", ["Documents", "Excel"], 3, 4096),
+#         File(8, "Backup.zip", ["Backup"], 233, 8192),
+#         File(13, "Presentation.pptx", ["Documents", "Presentation"], 3, 3072),
+#         File(21, "Video.mp4", ["Media", "Videos"], 34, 6144),
+#         File(34, "Folder2", ["Folder"], 3, 0),
+#         File(55, "Code.py", ["Programming"], -1, 1536),
+#         File(89, "Audio.mp3", ["Media", "Audio"], 34, 2560),
+#         File(144, "Spreadsheet2.xlsx", ["Documents", "Excel"], 3, 2048),
+#         File(233, "Folder3", ["Folder"], -1, 4096),
+#     ]
 
-    # assert largestFileSize(testFiles) == 20992
+#     assert sorted(leafFiles(testFiles)) == [
+#         "Audio.mp3",
+#         "Backup.zip",
+#         "Code.py",
+#         "Document.txt",
+#         "Image.jpg",
+#         "Presentation.pptx",
+#         "Spreadsheet.xlsx",
+#         "Spreadsheet2.xlsx",
+#         "Video.mp4"
+#     ]
+
+#     # assert kLargestCategories(testFiles, 3) == [
+#     #     "Documents", "Folder", "Media"
+#     # ]
+
+#     # assert largestFileSize(testFiles) == 20992
