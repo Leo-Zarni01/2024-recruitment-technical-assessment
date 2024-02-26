1a83cbcfb35b248ca64d11119ec8b40a56650a34
Leo-Zarni01 <leokhantzarni@gmail.com>
2024-02-26 23:45:10 +1100

    removed commented codes to draft file

--- backend/py/task.py
+++ backend/py/task.py
@@ -166,32 +166,6 @@ testFiles = [
         File(233, "Folder3", ["Folder"], -1, 4096),
         ]
 
-graph = convert_files_to_graph(testFiles)
-# graph.print_graph()
-
-# testFiles = [
-#         File(1, "Document.txt", ["Documents"], 3, 1024),
-#         File(2, "Image.jpg", ["Media", "Photos"], 34, 2048),
-#         File(3, "Folder", ["Folder"], -1, 0),
-#         File(5, "Spreadsheet.xlsx", ["Documents", "Excel"], 3, 4096),
-#         File(8, "Backup.zip", ["Backup"], 233, 8192),
-#         File(13, "Presentation.pptx", ["Documents", "Presentation"], 3, 3072),
-#         File(21, "Video.mp4", ["Media", "Videos"], 34, 6144),
-#         File(34, "Folder2", ["Folder"], 3, 0),
-#         File(55, "Code.py", ["Programming"], -1, 1536),
-#         File(89, "Audio.mp3", ["Media", "Audio"], 34, 2560),
-#         File(144, "Spreadsheet2.xlsx", ["Documents", "Excel"], 3, 2048),
-#         File(233, "Folder3", ["Folder"], -1, 4096),
-#     ]
-
-
-# testFiles2 = [
-#     File(3, "Folder", ["Folder"], -1, 0),
-#     File(1, "Document.txt", ["Documents"], 3, 1024),
-# ]
-
-# graph_2 = convert_files_to_graph(testFiles2)
-# graph_2.print_graph()
 
 if __name__ == '__main__':
     testFiles = [
