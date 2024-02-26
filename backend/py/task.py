55ac7070fd2cc5a37aa1f98b8a7bda45ab18c6fa
Leo-Zarni01 <leokhantzarni@gmail.com>
2024-02-26 23:46:47 +1100

    tidied task.py

--- backend/py/task.py
+++ backend/py/task.py
@@ -79,8 +79,6 @@ def find_categories(files: list[File]) -> dict:
 def sort_categories(categories_dict: dict) -> dict:
     return sorted(categories_dict.items(), key=lambda x: (-x[1], x[0]))
 
-
-
 def dfs(g: Graph, lookup_file: File) -> int:
     files_visited = {}
     for i in range(g.size()):
@@ -150,22 +148,6 @@ def largestFileSize(files: list[File]) -> int:
         # dfs(file_graph, each_file)
         sizes.append(dfs(file_graph, each_file))
     return max(sizes)
-
-testFiles = [
-        File(1, "Document.txt", ["Documents"], 3, 1024),
-        File(3, "Folder", ["Folder"], -1, 0),
-        File(5, "Spreadsheet.xlsx", ["Documents", "Excel"], 3, 4096),
-        File(8, "Backup.zip", ["Backup"], 233, 8192),
-        File(13, "Presentation.pptx", ["Documents", "Presentation"], 3, 3072),
-        File(21, "Video.mp4", ["Media", "Videos"], 34, 6144),
-        File(34, "Folder2", ["Folder"], 3, 0),
-        File(2, "Image.jpg", ["Media", "Photos"], 34, 2048),
-        File(55, "Code.py", ["Programming"], -1, 1536),
-        File(89, "Audio.mp3", ["Media", "Audio"], 34, 2560),
-        File(144, "Spreadsheet2.xlsx", ["Documents", "Excel"], 3, 2048),
-        File(233, "Folder3", ["Folder"], -1, 4096),
-        ]
-
 
 if __name__ == '__main__':
     testFiles = [
